b24b84aaece4ed1ddb4072e04db2665348385857
Ratin Gao <ratin@knsoft.org>
2023-07-01 17:28:59 +0800

    [Rtl/Array] Fix incorrect size of item used in CombineArray()

--- Source/Rtl/Array.cs
+++ Source/Rtl/Array.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
+using System.Runtime.InteropServices;
 
 namespace KNSoft.C4Lib;
 
@@ -13,13 +14,14 @@ static partial class Rtl
         return NewArray;
     }
 
-    public static T[] CombineArray<T>(params T[][] Arrays)
+    public static T[] CombineArray<T>(params T[][] Arrays) where T : new()
     {
         T[] NewArray = new T[Arrays.Sum(x => x.Length)];
         Int32 Offset = 0;
+        Int32 ItemSize = Marshal.SizeOf(new T().GetType());
         foreach (T[] ArrayItem in Arrays)
         {
-            System.Buffer.BlockCopy(ArrayItem, 0, NewArray, Offset, ArrayItem.Length);
+            System.Buffer.BlockCopy(ArrayItem, 0, NewArray, Offset, ArrayItem.Length * ItemSize);
             Offset += ArrayItem.Length;
         }
         return NewArray;
